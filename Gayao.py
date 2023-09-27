350f76c70b77e9bbe431cacff72e149102d8ac29
yjcgayao <145944404+yjcgayao@users.noreply.github.com>
2023-09-27 08:47:31 +0800

    Add files via upload

---
+++ Gayao.py
@@ -0,0 +1,14 @@
+name = input ("What is your name?")
+age= input ("What is your age?")
+age = int(age)
+age_next_year = age + 1
+
+pi = 3.14
+A = 5%3
+B = 5*3
+
+print (f"Hello, {name}")
+print (f"Your age next year is, {(age_next_year)}")
+print (f"Pi Value is {pi}")
+print (f"Modulo of 5 & 3 is {A}")
+print (f"5 x 3 is {B}")
