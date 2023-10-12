55e03c4dfb6ad4a1205b25735d6d243a5197b155
Yasmien Gayao <145944404+yjcgayao@users.noreply.github.com>
2023-10-13 04:42:32 +0800

    Update summation.py

--- labact3-loops/summation.py
+++ labact3-loops/summation.py
@@ -1,11 +1,11 @@
 # Gayao, Yasmien Jen C.
 # BSCS 1B
 def pattern(n):
-    result, formula = 0, ''
-    for i in range(1, n + 1):
-        result += i
-        formula += str(i) + '+' if i < n else str(i)
-    print(f"input: {n}")
-    print(f"formula: {formula}")
-    print(f"output: {result}")
+  result, formula = 0, ""
+  for i in range(1, n + 1):
+    result += i
+    formula += f"{i}{'+' if i < n else ''}"
+  print(f"input: {n}")
+  print(f"formula: {formula}")
+  print(f"output: {result}")
 pattern(4)
