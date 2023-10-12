d65400e723f7df10866ed5de5cc0227d80075f42
Yasmien Gayao <145944404+yjcgayao@users.noreply.github.com>
2023-10-13 04:59:50 +0800

    Update inverse_right_triangle.py

--- labact3-loops/inverse_right_triangle.py
+++ labact3-loops/inverse_right_triangle.py
@@ -1,7 +1,6 @@
 # Gayao, Yasmien jen C.
 # BSCS 1-B     CC2
-rows = 5
-for i in range(rows, 0, -1):
-    for y in range(1, i + 1):
-        print(y, end=' ')
-    print("\r")
+def print_inverted_triangle(rows):
+  for i in range(rows, 0, -1):
+    print(*range(1, i + 1), sep=' ')
+print_inverted_triangle(5)
