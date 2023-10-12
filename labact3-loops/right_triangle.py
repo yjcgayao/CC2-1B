4c32015a6f4b0651623da6be44959be56b7cd6e1
Yasmien Gayao <145944404+yjcgayao@users.noreply.github.com>
2023-10-13 05:01:43 +0800

    Update right_triangle.py

--- labact3-loops/right_triangle.py
+++ labact3-loops/right_triangle.py
@@ -1,11 +1,11 @@
 # Gayao, Yasmien jen C.
 # BSCS 1-B     CC2
-def print_row(rows):
-  """Prints a row of numbers from 1 to the given row number.
+def print_inverted_triangle(rows):
+  """Prints an inverted triangle of numbers from 1 to the given row number.
   Args:
     rows: The number of rows to print.
   """
-  print(*range(1, rows + 1))
-# Print a right triangle with 5 rows.
-for i in range(5):
-  print_row(i + 1)
+  for i in range(rows, 0, -1):
+    print(*range(1, i + 1), sep=' ')
+# Print an inverted triangle with 5 rows.
+print_inverted_triangle(5)
